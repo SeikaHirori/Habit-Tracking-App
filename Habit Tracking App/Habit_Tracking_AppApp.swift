656f6938c58cd16a5196b798dfaed8ef871731df
SeikaHirori <SeikaHirori@outlook.com>
2023-05-14 13:02:14 -0700

    Initial Commit

---
+++ Habit Tracking App/Habit_Tracking_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_Tracking_AppApp.swift
+//  Habit Tracking App
+//
+//  Created by Seika Hirori on 5/14/23.
+//
+
+import SwiftUI
+
+@main
+struct Habit_Tracking_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
