23208e7e6b6d374c48f969c2d1a67b1259b53831
SeikaHirori <SeikaHirori@outlook.com>
2023-05-17 12:42:54 -0700

    Updated ActivityView to contain info form Activity data

--- Habit Tracking App/Views/ActivitiesView.swift
+++ Habit Tracking App/Views/ActivitiesView.swift
@@ -13,19 +13,35 @@ struct ActivitiesView: View {
     
     
     var body: some View {
-        return VStack {
-            Text("Hello, Activities View!")
-            
-            List(activities.activities, id: \.self) { activity in
-                Button("Show Activity") {
-                    showingSheet.toggle()
-                }
-                .sheet(isPresented: $showingSheet) {
-                    ActivityView()
-                }
-
-            }
+        return NavigationStack {
+            VStack {
+//                Text("Hello, Activities View!")
+                
+                if activities.activities.isEmpty == true {
+                    Text("empty ;'[")
+                } else {
+                    List(activities.activities, id: \.self) { currentActivity in
+                        Button("Show Activity") {
+                            showingSheet.toggle()
+                        }
+                        .sheet(isPresented: $showingSheet) {
+                            ActivityView(activity: currentActivity)
+                        }
                         
+                    }
+                }
+            }
+            .navigationTitle("Activities")
+            .toolbar {
+                Button {
+                    let newActivitiy = Activity(title: "sleep", description: "zzz", amountCompletion: 0)
+                    
+                    activities.activities.append(newActivitiy)
+                } label: {
+                    Image(systemName: "plus")
+                }
+            }
+            .padding()
         }
     }
 }
--- Habit Tracking App/Views/ActivityView.swift
+++ Habit Tracking App/Views/ActivityView.swift
@@ -9,19 +9,24 @@ import SwiftUI
 
 struct ActivityView: View {
     @Environment(\.dismiss) var dismiss
+    let activity:Activity
     
     var body: some View {
         return VStack(spacing: 30) {
-            Text("Hello from Activity View!")
+//            Text("Hello from Activity View!")
 
             
             VStack(spacing: 20) {
-                Text("Activity Title")
+//                Text("Activity Title")
+                Text(activity.title)
                     .font(.headline)
                 
-                Text("Activity Description")
+//                Text("Activity Description")
+                Text(activity.description)
                 
-                Text("Activity's completion amount")
+//                Text("Activity's completion amount")
+                Text("Completion Amount: \(activity.amountCompletion)")
+                
             }
             Button("Dismiss") {
                 dismiss()
@@ -31,7 +36,9 @@ struct ActivityView: View {
 }
 
 struct ActivityView_Previews: PreviewProvider {
+    static let previewActivity: Activity =  Activity(title: "Walking", description: "Walk while playing Pokemon Go", amountCompletion: 1)
+    
     static var previews: some View {
-        ActivityView()
+        ActivityView(activity: previewActivity)
     }
 }
