b3aa3e5d2fc5d0018611df37af3d1fe704b92373
SeikaHirori <SeikaHirori@outlook.com>
2023-05-17 18:33:03 -0700

    Changes: - Added increase and decrease to amountCompletion - Added func "amountIsEmpty() - Added .onDelete to buttons of each activity
    Bug:
    - changes to var "amountCompletion" is not being saved

--- Habit Tracking App/Data/Activity.swift
+++ Habit Tracking App/Data/Activity.swift
@@ -13,4 +13,17 @@ struct Activity: Codable, Identifiable, Equatable, Hashable {
     let description: String
     var amountCompletion: Int
     
+    mutating func increaseAmount() -> Void {
+        amountCompletion += 1
+    }
+    
+    mutating func decreaseAmount() -> Void {
+        if !amountIsEmpty() {
+            amountCompletion -= 1
+        }
+    }
+    
+    func amountIsEmpty() -> Bool {
+        return amountCompletion <= 0
+    }
 }
